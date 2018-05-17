57d9e477ce78581c62313770007d06bf8e5b9b88
Taryn <taryn@sugarpush.com.au>
2018-05-17 10:57:10 +1000

    removed private from api controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,19 +2,4 @@ class ApiController < ApplicationController
 
   include Knock::Authenticable
 
-  #before_action :set_default_format
-
-  # before_action :set_default_format
-
-
-  private
-    # def set_default_format
-    #   request.format = :json
-    # end
-
-    # Define unauthorized access json response
-    def unauthorized_entity(entity_name)
-        render json: { error: "Unauthorized request" }, status:
-        :unauthorized
-    end
 end
